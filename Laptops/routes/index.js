ed2ac735efe91d4aca7b533b521c61a3d3c59043
Alex Eseyin <lexbrayan@gmail.com>
2021-09-16 13:50:27 -0400

    created a new path '/laptops'
    the /laptops path currently returns a JSON of all the laptops available
    
    Note: this will be refactored to have '/laptops/all

--- Laptops/routes/index.js
+++ Laptops/routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const createError = require('http-errors');
+const express = require('express');
+const router = express.Router();
+
+const laptops=require('../modules/laptops');
+const url =require('url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Laptop BackEnd Service'});
 });
+
+router.get('/laptops', (request, response, next) => {
+  let get_params = url.parse(request.url, true).query;
+  console.log('Processing laptops');
+  if (Object.keys(get_params).length == 0) {
+  console.log('no params');
+  response.setHeader('content-type', 'application/json');
+  response.end(JSON.stringify(laptops.list()));
+  } else {
+  // get first parameter only
+  let key = Object.keys(get_params)[0];
+  console.log("First key is: " + key);
+  let value = request.query[key];
+  console.log('params ' + value);
+  let result = laptops.query_by_arg(key, value);
+  if (result) {
+  response.setHeader('content-type', 'application/json');
+  response.end(JSON.stringify(result));
+  } else {
+  next(createError(404));
+  }
+  }
+ });
 
 module.exports = router;
