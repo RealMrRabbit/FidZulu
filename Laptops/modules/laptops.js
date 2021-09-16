6873b1919b7f73603e6b6411e303f4a7fbb7ff85
Jacob DiSpirito <41126174+jdispirito21@users.noreply.github.com>
2021-09-16 14:48:21 -0400

    Added functionality to change sales tax based on location

--- Laptops/modules/laptops.js
+++ Laptops/modules/laptops.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 let read_json_file = () => {
- let file = './data/laptops.json';
- return fs.readFileSync(file);
+    let file = './data/laptops.json';
+    return fs.readFileSync(file);
 }
-exports.list = function() {
- return JSON.parse(read_json_file());
+
+exports.list = () => {
+    return JSON.parse(read_json_file());
 };
-exports.query_by_arg = (arg, value) => {
- let json_result = JSON.parse(read_json_file());
- // all addresses are stored in a "result" object
- let result = json_result.result;
- console.log("query by arg: " + arg + " " + value);
- for (let i = 0; i < result.length; i++) {
- let laptop = result[i];
- if (laptop[arg] === value) {
- return laptop;
- }
- }
- return null;
-};
+
+
+exports.calcTax = (tax) => {
+    let data = JSON.parse(read_json_file());
+    for (let i = 0; i < data.length; ++i) {
+      let addedTax = data[i].price * tax;
+      data[i].price = parseFloat((data[i].price + addedTax).toFixed(2));
+    }
+    return data;
+  }
